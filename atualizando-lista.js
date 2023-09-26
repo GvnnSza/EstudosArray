73e5e9734a1ef0bc5f3e9bca05b2d8a242fd84c7
Giovani De Souza Marques <giovani.marques@iesb.edu.br>
2023-09-26 11:45:24 -0300

    Java Array
    Estudo de programacao Array Java

--- atualizando-lista.js
+++
@@ -1,9 +0,0 @@
-// Todo tirar Ana e caio da lista e colocar Rodrigo
-
-const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
-
-nomes.splice(1, 2, "Rodrigo");
-
-//! PODEMOS SIM USAR O nome.push("RODRIGO"), mas podemos colocar-lo dentro do splice que ele vai substituir os 2 indices e colocar um novo na lista
-
-console.log(nomes);
